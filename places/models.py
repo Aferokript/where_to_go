955f25ca8617360fe82f07f728fc38d26dea253b
Aferokript <alberto.cardinale@bk.ru>
2026-09-15 17:33:45 +0500

    add place model

---
+++ places/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=40)
+    description_short = models.TextField(blank=True)
+    description_long = models.TextField(blank=True)
+    lon = models.FloatField()
+    lat = models.FloatField()
+    
+    def __str__(self):
+        return self.title
+    
+
+class Image(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField()
+    
+    
