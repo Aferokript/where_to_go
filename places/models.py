beab4c4c65b1edfcbeb44d1ec5b4ec408a4af776
Aferokript <alberto.cardinale@bk.ru>
2026-09-15 16:00:44 +0300

    Add Image model

--- places/models.py
+++ places/models.py
@@ -14,6 +14,10 @@ class Place(models.Model):
 
 class Image(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField()
+    image = models.ImageField(upload_to='places/')
+    
+    
+    def __str__(self):
+        return f'{self.place} {self.image}'
     
     
