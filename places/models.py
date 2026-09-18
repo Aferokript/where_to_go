3790c6c88dbdd90339390360866df1686a8f3dbe
Aferokript <alberto.cardinale@bk.ru>
2026-09-18 20:38:40 +0300

    cut necessary to numeration on admin page №2

--- places/models.py
+++ places/models.py
@@ -8,6 +8,16 @@ class Place(models.Model):
     lon = models.FloatField()
     lat = models.FloatField()
     
+    place_for_order = models.PositiveIntegerField(
+        default = 0,
+        blank = False,
+        null = False
+    )
+    
+    class Meta:
+        ordering = ['place_for_order']
+        
+    
     def __str__(self):
         return self.title
     
@@ -15,9 +25,16 @@ class Place(models.Model):
 class Image(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='places/')
+    image_for_order = models.PositiveIntegerField(
+        blank = False,
+        null = False
+    )
+    
+    
+    class Meta():
+        ordering = ['image_for_order']
     
     
     def __str__(self):
         return f'{self.place} {self.image}'
-    
     
