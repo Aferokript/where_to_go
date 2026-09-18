9eed2894757beb115ed5cf6f03522f8fb3187257
Aferokript <alberto.cardinale@bk.ru>
2026-09-18 13:57:25 +0300

    add showing image on admin page

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Place, Image
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 
 class ImageInline(admin.TabularInline):
@@ -15,6 +17,16 @@ class PlaceAdmin(admin.ModelAdmin):
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('place', 'image')
+    list_display = ('place', 'place_image')
+    
+    
+    def place_image(self, obj):
+        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
+            url = obj.image.url,
+            width=obj.image.width,
+            height=obj.image.height,
+            )
+    )
+   
 
 
