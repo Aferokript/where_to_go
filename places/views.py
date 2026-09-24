82bedcc04e28b44ce6c48dbc712e3be5ac002da6
Aferokript <alberto.cardinale@bk.ru>
2026-09-24 13:45:42 +0300

    delete json dumps

--- places/views.py
+++ places/views.py
@@ -1,54 +1,51 @@
-import requests
-from django.core.management.base import BaseCommand
-from django.core.files.base import ContentFile
-from places.models import Place, Image
+from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from django.urls import reverse
+from .models import Place
 
 
-class Command(BaseCommand):
+def index(request):
+    features = []
 
-    def add_arguments(self, parser):
-        parser.add_argument('url')
+    for place in Place.objects.all():
+        features.append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lon, place.lat],
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": reverse("place_detail", args=[place.id])
+            },
+        })
+
+    place_data = {
+        "type": "FeatureCollection",
+        "features": features,
+    }
+
+    context = {"place_data": place_data}
+    return render(request, 'index.html', context)
 
 
-    def handle(self, *args, **options):
-        try:
-            response = requests.get(options['url'])
-            self.stdout.write('Скачиваем...')
-            response.raise_for_status()
-        except requests.RequestException as error:
-            self.stderr.write(f'Ошибка {error}')
-            return
-
-        place_info = response.json()
-
-        place, created = Place.objects.get_or_create(
-            title=place_info.get('title'),
-            short_description=place_info.get('description_short'),
-            long_description=place_info.get('description_long'),
-            lat=float(place_info.get('coordinates', {}).get('lat')),
-            lon=float(place_info.get('coordinates', {}).get('lng')),
-        )
-
-        for index, imgs_url in enumerate(place_info.get('imgs', [])):
-            filename = imgs_url.split('/')[-1]
-            if Image.objects.filter(place=place, image__endswith=filename).exists():
-                self.stdout.write(f'Уже есть: {filename}')
-                continue
-            try:
-                image_response = requests.get(imgs_url)
-                image_response.raise_for_status()
-            except requests.RequestException as error:
-                self.stderr.write(f'Ошибка {imgs_url}: {error}')
-                continue
-
-            image_data = image_response.content
-            image = Image(place=place, image_order=index)
-            image.image.save(
-                filename,
-                ContentFile(image_data),
-                save=True,
-            )
-            
+def place_detail(request, place_id):
+    place = get_object_or_404(Place.objects.prefetch_related('images'), id=place_id)
+    place_data = {
+        "title": place.title,
+        "imgs": [img.image.url for img in place.images.all()],
+        "description_short": place.short_description,
+        "description_long": place.long_description,
+        "coordinates": {
+            "lat": place.lat,
+            "lng": place.lon,
+        },
+    }
+    
+    return JsonResponse(place_data, json_dumps_params={'ensure_ascii': False})
+    
     
 
 
